94ded6595739d624b57d68978061a1e3fe529bfd
omgjr <omgurjar9244@gmail.com>
2026-05-18 20:12:13 +0530

    arithmetic operation program

---
+++ src/Basic Program/Arithmetic.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to arithmetic operations");
+
+        System.out.println("Please enter first number");
+        int first = input.nextInt();
+
+        System.out.println("Please enter second number");
+        int second = input.nextInt();
+
+        int add = first + second;
+        int sub = first - second;
+        int mul = first * second;
+        int div = first / second;
+        int mod = first % second;
+
+        System.out.println("Addition is " + add);
+        System.out.println("Subtraction is " + sub);
+        System.out.println("Multiplication is " + mul);
+        System.out.println("Division is " + div);
+        System.out.println("Modulus is " + mod);
+
+        // input.close();
+    }
+}
