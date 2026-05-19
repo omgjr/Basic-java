b51822c29e5fbb7283776a95e2b14d61cb9d881d
omgjr <omgurjar9244@gmail.com>
2026-05-19 19:13:51 +0530

    local variable program

---
+++ src/LocalVariable.java
@@ -0,0 +1,13 @@
+//package com.studyofindia;
+
+ class LocalVariable {
+	void show() {
+		int a=50;
+		System.out.println(a);
+	}
+	public static void main(String[] args) {
+		LocalVariable obj=new LocalVariable();
+		obj.show();
+	}
+
+}
