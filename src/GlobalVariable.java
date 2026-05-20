353e070939bcb7792377dbc216ab33aa4a69dc14
omgjr <omgurjar9244@gmail.com>
2026-05-20 18:21:14 +0530

    Global variable

---
+++ src/GlobalVariable.java
@@ -0,0 +1,11 @@
+public class GlobalVariable {
+    int marks=98;
+    public static void main(String[] args) {
+        GlobalVariable obj=new GlobalVariable();
+        System.out.println(obj.marks);
+
+    
+    }
+}
+    
+
