f40a134d0f066e1663ade8f956ba8e0d739f2356
omgjr <omgurjar9244@gmail.com>
2026-06-26 19:03:28 +0530

    finding the odd and even number are thrre

---
+++ src/evenOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args)
+	{
+		System.out.println("Welcome to check number odd or even");
+		Scanner input=new Scanner(System.in);
+		System.out.print("Please enter any number: ");
+		int num=input.nextInt();
+		
+		
+		if(num%2==0) {
+			System.out.println("Your number is even");
+			
+		}else {
+			System.out.println("Your number is odd");
+			
+		}
+		
+	}
+
+    
+}
